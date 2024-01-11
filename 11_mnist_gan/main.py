9d4eacbade28836549dca321a2cae845c1b75053
bestswlkh0310 <hhhello0507@gmail.com>
2024-01-11 21:34:01 +0900

    11: create text image

--- 11_mnist_gan/main.py
+++ 11_mnist_gan/main.py
@@ -37,12 +37,23 @@ def create_discriminator():
 
 def create_gan(discriminator: Sequential, generator: Sequential):
     discriminator.trainable = False
-    gan_input = Input(shape=(100, ))
+    gan_input = Input(shape=(100,))
     x = generator(gan_input)
     gan_output = discriminator(x)
     gan = Model(inputs=gan_input, outputs=gan_output)
     gan.compile(loss='binary_crossentropy', optimizer='adam')
     return gan
+
+
+def plot_generated_images(generator: Sequential):
+    noise = np.random.normal(loc=0, scale=1, size=(100, 100))
+    generated_images = generator.predict(noise).reshape(100, 28, 28)
+    plt.figure(figsize=(10, 10))
+    for i in range(generated_images.shape[0]):
+        plt.subplot(10, 10, i + 1)
+        plt.imshow(generated_images[i], interpolation='nearest')
+        plt.axis('off')
+    # plt.tight_layout()
 
 
 generator = create_generator()
@@ -53,3 +64,29 @@ discriminator.summary()
 
 gan = create_gan(discriminator, generator)
 gan.summary()
+
+plot_generated_images(generator)
+
+batch_size = 128
+epochs = 5000
+
+for e in tqdm(range(epochs)):
+    noise = np.random.normal(0, 1, [batch_size, 100])
+    generated_images = generator.predict(noise)
+    image_batch = mnist_data[np.random.randint(low=0, high=mnist_data.shape[0], size=batch_size)]
+    X = np.concatenate([image_batch, generated_images])
+    y_dis = np.zeros(2 * batch_size)
+    y_dis[:batch_size] = 1
+
+    discriminator.trainable = True
+    discriminator.train_on_batch(X, y_dis)
+
+    noise = np.random.normal(0, 1, [batch_size, 100])
+    y_gen = np.ones(batch_size)
+    discriminator.trainable = False
+    gan.train_on_batch(noise, y_gen)
+
+    if e == 0 or e % 1000 == 0:
+        plot_generated_images(generator)
+
+plt.show()
