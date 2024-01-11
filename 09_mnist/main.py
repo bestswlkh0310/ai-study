22ac46f630a909e300e6f09908c2f6d65afb0f84
bestswlkh0310 <hhhello0507@gmail.com>
2024-01-11 14:59:46 +0900

    reshape & categorical

--- 09_mnist/main.py
+++ 09_mnist/main.py
@@ -5,8 +5,21 @@ from keras.datasets import mnist
 import numpy as np
 import matplotlib.pyplot as plt
 
+# load
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print('x_train_shape', x_train.shape)
 print('y_train_shape', y_train.shape)
 print('x_test_shape', x_test.shape)
 print('y_test_shape', y_test.shape)
+
+# reshape
+x_train = x_train.reshape(60000, 28 * 28).astype('float32') / 255
+x_test = x_test.reshape(10000, 28 * 28).astype('float32') / 255
+print(x_train.shape)
+print(x_test.shape)
+
+# categorical
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+print(y_train.shape)
+print(y_test.shape)
