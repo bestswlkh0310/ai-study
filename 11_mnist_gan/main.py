4aa1103310f0d7d67154b4cfe7e5d81b28032b97
bestswlkh0310 <hhhello0507@gmail.com>
2024-01-11 21:10:24 +0900

    11: create discriminator & generator, gan model

--- 11_mnist_gan/main.py
+++ 11_mnist_gan/main.py
@@ -13,3 +13,43 @@ mnist_data = x_test.reshape(10000, 28 * 28)
 print(mnist_data.shape)
 print(len(mnist_data))
 
+
+def create_generator():
+    generator = Sequential(name='create_text')
+    generator.add(Dense(256, input_dim=100))
+    generator.add(LeakyReLU(0.2))
+    generator.add(Dense(512))
+    generator.add(LeakyReLU(0.2))
+    generator.add(Dense(784, activation='tanh'))
+    return generator
+
+
+def create_discriminator():
+    discriminator = Sequential(name='text_discriminator')
+    discriminator.add(Dense(512, input_dim=784))
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(Dense(256))
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(Dense(1, activation='sigmoid'))
+    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.0002, beta_1=0.5))
+    return discriminator
+
+
+def create_gan(discriminator: Sequential, generator: Sequential):
+    discriminator.trainable = False
+    gan_input = Input(shape=(100, ))
+    x = generator(gan_input)
+    gan_output = discriminator(x)
+    gan = Model(inputs=gan_input, outputs=gan_output)
+    gan.compile(loss='binary_crossentropy', optimizer='adam')
+    return gan
+
+
+generator = create_generator()
+generator.summary()
+
+discriminator = create_discriminator()
+discriminator.summary()
+
+gan = create_gan(discriminator, generator)
+gan.summary()
